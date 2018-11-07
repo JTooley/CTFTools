ffa279a3829bfd547bbb07a6381dddf68984ed73
David Lomax <d.e.lomax@me.com>
2018-11-07 21:59:22 +0000

    Files for inceparaftption challenge.

---
+++ Dave/Inceparaftption/reassemble2.py
@@ -0,0 +1,23 @@
+
+f1 = open("1.bin", "rb");
+f2 = open("2.bin", "rb");
+f3 = open("3.bin", "rb");
+f4 = open("4.bin", "rb");
+f5 = open("5.bin", "rb");
+
+fout = open("secrets.zip", "wb");
+
+while True:
+  byte = f1.read(1);
+
+  if byte == '':
+    break;
+  fout.write(byte);
+  byte = f2.read(1);
+  fout.write(byte);
+  byte = f3.read(1);
+  fout.write(byte);
+  byte = f4.read(1);
+  fout.write(byte);
+  byte = f5.read(1);
+  fout.write(byte);
